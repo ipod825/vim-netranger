ba0475122f8acbcd6d66802da44f2a3f30fd6e7f
Shih-Ming (mingo) Wang <ipod825@gmail.com>
2018-01-11 22:38:09 -0800

    #4

--- pythonx/netranger/config.py
+++ pythonx/netranger/config.py
@@ -1,7 +1,7 @@
 import os
 import tempfile
 root_dir = os.path.expanduser('~/.netranger/')
-config_dir = '{}/../../../config'.format(os.path.dirname(__file__))
+config_dir = '{}/../../config'.format(os.path.dirname(__file__))
 test_remote_name = 'netrtest'
 test_dir = os.path.join(tempfile.gettempdir(), 'netrtest')
 test_local_dir = os.path.join(test_dir, 'local')
